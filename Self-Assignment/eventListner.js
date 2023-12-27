d7a80a9f14af2d1d64d484338f1f64dcfcba2466
Aakar <aakaar.bh@gmail.com>
2023-12-28 02:25:24 +0530

    dark mode light mode

---
+++ Self-Assignment/eventListner.js
@@ -0,0 +1,19 @@
+let btn=document.querySelector("button");
+        let body=document.querySelector("body");
+        let currMode="light";
+        
+        btn.addEventListener("click",()=>{
+            if(currMode === "light"){
+                currMode = "dark";
+                body.classList.add("dark");
+                body.classList.remove("light");
+                console.log("light");
+            }
+            
+            else{
+                currMode = "light";
+                body.classList.add("light");
+                body.classList.remove("dark");
+                console.log("dark");
+            }
+        })
