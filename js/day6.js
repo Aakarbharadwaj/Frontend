d498d5b7876c68ec9b685cb3d0031bfec22091b9
Aakar <aakaar.bh@gmail.com>
2023-12-28 16:50:34 +0530

    creatElement & appendClild

---
+++ js/day6.js
@@ -0,0 +1,12 @@
+let btn = document.querySelector("#btn");
+        // let heading = document.querySelector("h1");
+        console.log(name);
+        btn.onclick=function(){
+            let heading = document.createElement("h1");
+            // console.log(heading);
+            let name = document.querySelector("input").value;
+            heading.innerText = name;
+            // console.log(heading);
+            document.querySelector("#test").appendChild(heading);
+            // document.body.appendChild(heading);
+        }
