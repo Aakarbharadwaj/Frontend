d10addb471315ca60988afb3e559eae81eca9390
Aakar <aakaar.bh@gmail.com>
2023-12-28 19:01:50 +0530

    color the boxes

--- js/day4Assignment.js
+++ js/day4Assignment.js
@@ -2,11 +2,16 @@ document.getElementById("btn").onclick=function(){
     let num=document.querySelector("#box").value;
     let c=document.querySelector("#color").value;
     let l=document.querySelectorAll(".boxcolor");
-
-    for(let i=1  ; i<=l.length ; i++){
-        if(i==num){
-            l[i-1].style.background=c;
-        } 
+    
+    if(num<l.length){
+        for(let i=1  ; i<=l.length ; i++){
+            if(i==num){
+                l[i-1].style.background=c;
+            }  
+        }
     }
+    else{
+        alert("Enter a valid number");
+    } 
 }
 // document.querySelectorAll(".boxcolor")
