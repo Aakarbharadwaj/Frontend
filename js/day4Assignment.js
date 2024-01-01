d22e1d396e34a9424b14d09d8d43862118fcfca7
Aakar <aakaar.bh@gmail.com>
2024-01-01 16:31:09 +0530

    instagram feeds

--- js/day4Assignment.js
+++ js/day4Assignment.js
@@ -2,10 +2,12 @@ document.getElementById("btn").onclick=function(){
     let num=document.querySelector("#box").value;
     let c=document.querySelector("#color").value;
     let l=document.querySelectorAll(".boxcolor");
-    
-    if(num<l.length){
+    console.log(l);
+    if(num<=l.length){
         for(let i=1  ; i<=l.length ; i++){
             if(i==num){
+                console.log(i);
+                console.log(i-1);
                 l[i-1].style.background=c;
             }  
         }
