138fe64893537cd12a7f57b68e2a4bf57e194a5f
Aakar <aakaar.bh@gmail.com>
2024-01-06 17:53:05 +0530

    dom append prepend

---
+++ Self-Assignment/dom/append.js
@@ -0,0 +1,8 @@
+let box = document.createElement("div");
+box.innerHTML="this is new div";
+box.setAttribute("style","color:red");
+// document.querySelector(".container").append(box);
+// document.querySelector(".container").prepend(box);
+// document.querySelector(".container").before(box);
+// document.querySelector(".container").after(box);
+// document.querySelector(".container").replaceWith(box);
