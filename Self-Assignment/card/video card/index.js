4a3f2b97ecf6d6f62c510c23de69aef0799e5371
Aakar <aakaar.bh@gmail.com>
2024-01-09 11:52:54 +0530

    upload and update video card

--- Self-Assignment/card/video card/index.js
+++ Self-Assignment/card/video card/index.js
@@ -1,12 +1,31 @@
 
 function getCard(titel,name,views,time,month){
-    let card = document.createElement("div");
-    card.setAttribute("class","card");
-    let div2 = document.querySelector(".card");
-    let left =div2.createElement("div");
-    left.setAttribute("class","left");
+    let viewstr
+    if(views<1000){
+        viewstr = views;
+    }
+    else if(views>1000000){
+        viewstr = views/1000 + "M";
+    }
+    else{
+        viewstr = views/1000 + "K";
+    }
+    let html =`<div class="card">
+    <div class="left">
+        <div class="img">
+                    <img src="http://source.unsplash.com/150x100?coding" alt="" class="thumnail">
+                </div>
+        <div class="time"><p>${time}</p></div>
+    </div>
+    <div class="info">
+        <p class="title">${titel}</p>
+        <p class="card-flex">${name} . ${viewstr} views . ${month} months ago</p>
+    </div>
+</div>`
+
+document.querySelector(".container").innerHTML += html;
 }
 
 
-getCard("New dom video uploaded guys","Code with Aakar",900000,"2 month");
+getCard("Learn javaScript inn detail video #1","Code with Aakar",900000,"12:55",2);
 
